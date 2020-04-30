c85c1e29879dcd27f69b75d80636796eec3b63b5
siddhant2 <31253947+siddhant2@users.noreply.github.com>
2020-04-30 18:13:11 +0530

    Update Lab4aApplication.java

--- Lab4.1/src/main/java/vinay/cg/gla/spring/Lab4aApplication.java
+++ Lab4.1/src/main/java/vinay/cg/gla/spring/Lab4aApplication.java
@@ -1,4 +1,4 @@
-package vinay.cg.gla.spring;
+package siddhant.cg.gla.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
