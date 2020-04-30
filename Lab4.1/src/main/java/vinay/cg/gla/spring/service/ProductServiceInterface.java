5268a1b7b3bcc478fb5e13da7e61f59c2c259369
siddhant2 <31253947+siddhant2@users.noreply.github.com>
2020-04-30 18:12:32 +0530

    Update ProductServiceInterface.java

--- Lab4.1/src/main/java/vinay/cg/gla/spring/service/ProductServiceInterface.java
+++ Lab4.1/src/main/java/vinay/cg/gla/spring/service/ProductServiceInterface.java
@@ -2,7 +2,7 @@ package siddhant.cg.gla.spring.service;
 
 import java.util.List;
 
-import vinay.cg.gla.spring.entity.Product;
+import siddhant.cg.gla.spring.entity.Product;
 
 
 public interface ProductServiceInterface {
